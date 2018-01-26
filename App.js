a69f73ef6b844d35f9c239d90e903e6e1070ed33
Lois Di Qual <lois.diqual@gmail.com>
2018-01-25 22:44:48 -0800

    More

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, Text, View, NavigatorIOS, TouchableHighlight } from 'react-native'
+import { StyleSheet, Text, View, NavigatorIOS, TouchableHighlight, ScrollView, Image } from 'react-native'
+import Button from 'react-native-button'
 
 export default class NavigatorIOSApp extends React.Component {
   render() {
@@ -8,7 +9,7 @@ export default class NavigatorIOSApp extends React.Component {
       <NavigatorIOS
         initialRoute={{
           component: MyScene,
-          title: 'My Initial Scene',
+          title: 'Welcome Loïs',
         }}
         style={{flex: 1}}
       />
@@ -18,7 +19,6 @@ export default class NavigatorIOSApp extends React.Component {
 
 class MyScene extends React.Component {
   static propTypes = {
-    title: PropTypes.string.isRequired,
     navigator: PropTypes.object.isRequired,
   }
 
@@ -30,11 +30,74 @@ class MyScene extends React.Component {
 
   render() {
     return (
-      <View style={{flex: 1}}>
-        <Text>Current Scene: { this.props.title }</Text>
-        <TouchableHighlight onPress={this._onForward}>
-          <Text>Tap me to load the next scene</Text>
-        </TouchableHighlight>
+      <View style={{
+        flex: 1,
+        backgroundColor: '#ecf0f1',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+      }}>
+
+        <ScrollView>
+          <View style={{
+            flex: 1,
+            alignItems: 'stretch',
+            margin: 20,
+            borderRadius: 20,
+            backgroundColor: 'white',
+            paddingTop: 30,
+            paddingBottom: 30,
+          }}>
+            <Text style={{ marginLeft: 30, marginRight: 30, fontSize: 30, fontWeight: 'bold' }}>Your trip</Text>
+            
+            <Image
+              style={{ height: 200, marginTop: 20 }}
+              source={{ uri: 'https://a0.thelateroomsgroup.com/moonstick/images/placeholders/mapplaceholder.836efba1.jpg' }}
+            />
+            
+            <View style={{
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginTop: 20,
+              marginLeft: 20,
+              marginRight: 20,
+            }}>
+            
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{ uri: 'http://www.louisvillewebgroup.com/sites/default/files/styles/team-member/public/avatar-circle-blue.png?itok=UmUsBb4f' }}
+              />
+              <Text style={{ marginLeft: 20, fontSize: 25 }}>Eric</Text>
+            
+            </View>
+            
+            <View style={{
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginTop: 10,
+              marginLeft: 20,
+              marginRight: 20,
+              marginBottom: 20,
+            }}>
+            
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{ uri: 'http://www.teachersculptacademy.com/images/c4.jpg' }}
+              />
+              <Text style={{ marginLeft: 20, fontSize: 25 }}>Natasha</Text>
+            
+            </View>
+            
+            <Button
+              containerStyle={{padding:10, height:45, overflow:'hidden', borderRadius:6, backgroundColor: '#3498db', marginTop: 20, marginLeft: 20, marginRight: 20}}
+              disabledContainerStyle={{backgroundColor: 'grey'}}
+              style={{fontSize: 20, color: 'white' }}>
+              See trip
+            </Button>
+            
+          </View>
+        </ScrollView>
       </View>
     )
   }

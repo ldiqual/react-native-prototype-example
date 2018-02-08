9d0020b92fef29b911c7cb3507bbfe5648f33171
Lois Di Qual <lois.diqual@gmail.com>
2018-02-08 02:51:00 -0800

    Start linting a bit

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { StyleSheet, Text, View, NavigatorIOS, TouchableHighlight, ScrollView, Image } from 'react-native'
-import Button from 'react-native-button'
-import { TabNavigator } from 'react-navigation'
-import Icon from 'react-native-vector-icons/FontAwesome'
 import AppV1 from './v1/App'
 
-export default class NavigatorIOSApp extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <AppV1 />
-    );
+    )
   }
 }
